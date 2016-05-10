a4d7a8cc55017d5d33359d0dd3d4b467981a0b94
Elisabet Gonzalez <eli.jvn@gmail.com>
2016-05-10 10:31:43 -0600

    Files added

---
+++ TCP_Client/TCP_Client/Program.cs
@@ -0,0 +1,45 @@
+﻿/* File name: program.cs
+ * Description:
+ * TCP Client.
+ * Show a message if the client was connected succesfully.
+ * Date: May 10th, 2016.
+ * Version: 1.0.
+ * 
+ * History:
+ * 
+ */
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create socket to send data over TCP
+            Socket sclient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Socket server port
+            const int client_port = 4444;
+            //IP address
+            IPAddress client_ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            //Init a new instance for IPEndPoint class
+            IPEndPoint ep = new IPEndPoint(client_ip, client_port);
+
+            try
+            {
+                sclient.Connect(ep);
+                Console.WriteLine("Conectado.");
+            }
+            catch
+            {
+                Console.WriteLine("Error de conexión.");
+            }
+
+            Console.ReadKey();
+            sclient.Close();
+        }
+    }
+}
---
+++ TCP_Server/TCP_Server/Program.cs
@@ -0,0 +1,46 @@
+﻿/* File name: program.cs
+ * Description:
+ * TCP Server.
+ * Show a message if the client was connected succesfully.
+ * Date: May 10th, 2016.
+ * Version: 1.0.
+ * 
+ * History:
+ * 
+ */
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create socket to send data over TCP
+            Socket sserver = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Socket server port
+            const int server_port = 4444;
+            //Init a new instance for IPEndPoint class
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, server_port);
+
+            try
+            {
+                Console.WriteLine("Esperando conexión...");
+                sserver.Bind(ep);
+                sserver.Listen(5);
+                sserver.Accept();
+                Console.WriteLine("Conectado con el servidor.");
+            }
+            catch
+            {
+                Console.WriteLine("Error de conexión.");
+            }
+
+            Console.ReadKey();
+            sserver.Close();
+        }
+    }
+}
